f4c56138cc285b28be469135e4a9e9765b5b9a29
Jake Drahos <drahos.jake@gmail.com>
2012-10-13 16:11:42 -0500

    Finished implementing control button eventboxes
    Now I will change them all over to buttons without relief, because that looks
    better...

--- ui.rb
+++ ui.rb
@@ -2,7 +2,7 @@ require 'gtk2'
 
 class MainWindow < Gtk::Window
   def cb_play_pause
-    puts "Play Button Pressed"
+    puts "Button Pressed"
   end
 
   def initialize
@@ -15,13 +15,35 @@ class MainWindow < Gtk::Window
     @controlAlignment = Gtk::Alignment.new(0.5, 0.5, 0, 0)
     @controlAlignment.add(@controlBox)
  
-    playIcon = Gtk::Image.new(Gtk::Stock::MEDIA_PLAY, \
+    @playIcon = Gtk::Image.new(Gtk::Stock::MEDIA_PLAY, \
                               Gtk::IconSize::SMALL_TOOLBAR)
+    @pauseIcon = Gtk::Image.new(Gtk::Stock::MEDIA_PAUSE, \
+                              Gtk::IconSize::SMALL_TOOLBAR)
+    @nextIcon = Gtk::Image.new(Gtk::Stock::MEDIA_NEXT, \
+                              Gtk::IconSize::SMALL_TOOLBAR)
+    @previousIcon = Gtk::Image.new(Gtk::Stock::MEDIA_PREVIOUS, \
+                              Gtk::IconSize::SMALL_TOOLBAR)
+
     @playEventBox = Gtk::EventBox.new()
-    @playEventBox.add(playIcon)
+    @playEventBox.add(@playIcon)
     @playEventBox.events = Gdk::Event::BUTTON_PRESS_MASK
 
+    @pauseEventBox = Gtk::EventBox.new()
+    @pauseEventBox.add(@pauseIcon)
+    @pauseEventBox.events = Gdk::Event::BUTTON_PRESS_MASK
+
+    @nextEventBox = Gtk::EventBox.new()
+    @nextEventBox.add(@nextIcon)
+    @nextEventBox.events = Gdk::Event::BUTTON_PRESS_MASK
+
+    @previousEventBox = Gtk::EventBox.new()
+    @previousEventBox.add(@previousIcon)
+    @previousEventBox.events = Gdk::Event::BUTTON_PRESS_MASK
+
     @controlBox.pack_start(@playEventBox, false, false, 0)
+    @controlBox.pack_start(@pauseEventBox, false, false, 0)
+    @controlBox.pack_start(@nextEventBox, false, false, 0)
+    @controlBox.pack_start(@previousEventBox, false, false, 0)
 
     @mainVBox.pack_start(@controlAlignment, false, false, 0)
     @mainVBox.pack_start(Gtk::HSeparator.new, false, false, 0)
@@ -30,8 +52,19 @@ class MainWindow < Gtk::Window
 
     @playEventBox.realize
     @playEventBox.window.cursor = Gdk::Cursor.new(Gdk::Cursor::HAND1)
-
     @playEventBox.signal_connect('button_press_event') { cb_play_pause }
+
+    @pauseEventBox.realize
+    @pauseEventBox.window.cursor = Gdk::Cursor.new(Gdk::Cursor::HAND1)
+    @pauseEventBox.signal_connect('button_press_event') { cb_play_pause }
+
+    @nextEventBox.realize
+    @nextEventBox.window.cursor = Gdk::Cursor.new(Gdk::Cursor::HAND1)
+    @nextEventBox.signal_connect('button_press_event') { cb_play_pause }
+
+    @previousEventBox.realize
+    @previousEventBox.window.cursor = Gdk::Cursor.new(Gdk::Cursor::HAND1)
+    @previousEventBox.signal_connect('button_press_event') { cb_play_pause }
 
   end
 end
